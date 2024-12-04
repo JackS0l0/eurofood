2827a4ee2741d2e0942260e269e2320988441973
JackS0l0 <you@example.com>
2024-12-04 06:05:23 +0400

    modified:   products/__pycache__/views.cpython-312.pyc 	modified:   products/views.py 	modified:   static/css/style.css 	modified:   static/css/style.css.map 	modified:   static/css/style.scss 	modified:   staticfiles/css/style.css 	modified:   staticfiles/css/style.css.map 	modified:   staticfiles/css/style.scss 	modified:   templates/allproducts.html 	modified:   templates/brands.html 	modified:   templates/categories.html

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import DetailView,ListView
 from .models import Categories,Brands,Products
+from django.core.paginator import Paginator
 class ProductDetailViews(DetailView):
     model = Products
     template_name = 'post.html'
@@ -12,37 +13,47 @@ class ProductDetailViews(DetailView):
         data['brands']=Brands.objects.all().order_by('name')
         data['products']=Products.objects.all().order_by('-date')
         return data
-def CategoriesList(request, category_id):
+def CategoriesList(request, category_id, page_number=1):
     if category_id:
         category = Categories.objects.get(id=category_id)
-        products = Products.objects.filter(category=category)
+        products = Products.objects.filter(category=category).order_by('-date')
     else:
         products = Products.objects.all().order_by('-date')
-    data={
+    pagi = Paginator(products, 15)
+    page_number = request.GET.get('page')
+    page_pagi = pagi.get_page(page_number)
+    data = {
         'title': 'Categories',
         'categories': Categories.objects.all().order_by('name'),
         'brands': Brands.objects.all().order_by('name'),
-        'products': products,
+        'products': page_pagi,
     }
-    return render(request, 'categories.html',data)
-def BrandsList(request, brand_id):
+    return render(request, 'categories.html', data)
+def BrandsList(request, brand_id, page_number=1):
     if brand_id:
         brand = Brands.objects.get(id=brand_id)
-        products = Products.objects.filter(brand=brand)
+        products = Products.objects.filter(brand=brand).order_by('-date')
     else:
         products = Products.objects.all().order_by('-date')
-    data={
-        'title': 'Categories',
+    pagi = Paginator(products, 15)
+    page_number = request.GET.get('page')
+    page_pagi = pagi.get_page(page_number)
+    data = {
+        'title': 'Brands',
         'categories': Categories.objects.all().order_by('name'),
         'brands': Brands.objects.all().order_by('name'),
-        'products': products,
+        'products': page_pagi,
     }
-    return render(request, 'categories.html',data)
-def products(request):
+    return render(request, 'brands.html', data)
+def products(request, category_id=None, page_number=1):
+    products=Products.objects.filter(article_category_id=category_id).order_by('-date') if category_id else Products.objects.all().order_by('-date')
+    pagi = Paginator(products, 15)
+    page_number = request.GET.get('page')
+    page_pagi = pagi.get_page(page_number)
     data={
         'title': 'All Products',
         'categories': Categories.objects.all().order_by('name'),
         'brands': Brands.objects.all().order_by('name'),
-        'products': Products.objects.all().order_by('-date'),
+        'products': page_pagi
     }
     return render(request, 'allproducts.html',data)
